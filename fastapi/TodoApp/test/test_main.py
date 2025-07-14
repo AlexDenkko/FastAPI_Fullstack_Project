b70486602cef3634ade63db29e936d46b862f0ac
svira <150971393+AlexDenkko@users.noreply.github.com>
2025-07-14 18:31:15 +0300

    fixed test_main.py file

--- fastapi/TodoApp/test/test_main.py
+++ fastapi/TodoApp/test/test_main.py
@@ -1,12 +1,11 @@
 from fastapi.testclient import TestClient # tämä tuo FastAPI:n testausasiakkaan
 from TodoApp.main import app # tuo TodoAppin pääsovelluksen
-from ..main import app # tuo pääsovelluksen, joka on määritelty main.py-tiedostossa
 from fastapi import status # tämä tuo HTTP-tilakoodit
 
 
 client = TestClient(app) # luo testausasiakkaan pääsovellukselle
 
-def test_health_check():
+def test_health_check(): # testaa terveystarkistusta
     response = client.get("/healthy") # tekee GET-pyynnön terveystarkistukseen
     assert response.status_code == status.HTTP_200_OK # tarkistaa, että vastauskoodi on 200 OK
     assert response.json() == {"status": "healthy"} # tarkistaa, että vastaus on odotettu JSON-objekti
