b294af10c0210f221d0edaadf31bb7fe503651e7
svira <150971393+AlexDenkko@users.noreply.github.com>
2025-08-06 18:18:36 +0300

    added endpoint in main.py for html file

--- fastapi/TodoApp/main.py
+++ fastapi/TodoApp/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI # tämä tuo FastAPI-sovelluskehyksen
+from fastapi import FastAPI, Request # tämä tuo FastAPI-sovelluskehyksen
 from .models import Base # tämä tuo mallit, jotka määrittelevät tietokannan rakenteen
 from .database import engine # tämä tuo moottorin tietokantayhteyttä varten
 from .routers import auth, todos, admin, users # tämä tuo reitittimet eri toiminnallisuuksia varten
+from fastapi.templating import Jinja2Templates # tämä tuo Jinja2-mallipohjat
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 # Luo kaikki taulut tietokannassa, jotka on määritelty malleissa
+
+templates = Jinja2Templates(directory="TodoApp/templates") # määrittelee mallipohjat hakemistosta
+
+
+@app.get("/") # tämä on pääsivu, joka palauttaa tervetuloviestin
+def test(request: Request): 
+    return templates.TemplateResponse("home.html", {"request": request}) # palauttaa home.html-mallipohjan, joka sijaitsee TodoApp/templates-hakemistossa
 
 @app.get("/healthy")
 def health_check():
